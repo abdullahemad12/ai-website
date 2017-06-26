dd0ed00089c8745ca5a35887806972afa1849d4e
abdullahemad12 <user.name>
2017-06-26 21:09:23 +0200

    Created the layout

---
+++ app/Http/Controllers/temp.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+	
+	$password = readline("Type your password here: ")
+  echo('password' => Hash::make($request->newPassword))
+
+?>
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,67 @@
+<!--Default header for the webiste Note: the footer must be added to page-->
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	    <!-- CSRF Token -->
+	    <meta name="csrf-token" content="{{ csrf_token() }}">
+		<title>{{ config('app.name', 'Laravel') }}</title>
+		<!--Css and javascript includes-->
+		<link href = "/css/bootstrap.min.css" rel = "stylesheet">
+		<link href = "/css/default.css" rel = "stylesheet">
+		<script src = "/js/framework/jquery-3.1.1.min.js" type="text/javascript"></script>
+		<script src = "/js/framework/bootstrap.min.js" type="text/javascript"></script>
+	</head>
+	<body>
+		
+		<nav class = "navbar navbar-default navbar-fixed-top">
+			<div class = "container-fluid">
+				<div class="navbar-header">
+					<a  class = "navbar-brand" href = "#"><img src = "/img/art/logo.png"></a>
+				</div>
+				<ul class = "nav navbar-nav navbar-right">
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Courses</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="#">About Us</a></li>
+					<li class="dropdown">
+				        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-cog"></span><span id = "account">Account</span>
+				        <span class="caret"></span></a>
+				        <ul class="dropdown-menu">
+				        @if(Auth::check())
+					        <li id = "bord"><a href="#"><b><span class="glyphicon glyphicon-user"></span> Profile</b></a></li>
+					        <li id = "bord"><a href="#"><b><span class="glyphicon glyphicon-pencil"></span> Add Course</b></a></li>
+					        <li id = "bord"><a href="#"><b><span class="glyphicon glyphicon-pencil"></span> Add Event</b></a></li>
+					        <li id ="bord"><a href="#"><b><span class="glyphicon glyphicon-pencil"></span> Add Project</b></a></li>
+					        <li id = "bord"><a href="#"><b><span class="glyphicon glyphicon-pencil"></span> Manage members</b></a></li>
+					   		<li><a href="#"><b><span class="glyphicon glyphicon-log-out"></span> Logout</b></a></li>
+					   	@else
+					   		<li><a href="/login"><b><span class="glyphicon glyphicon-log-in"></span> Log in</b></a></li>
+					   	@endif
+        				</ul>
+     				</li>
+				</ul>
+			</div>
+		</nav>
+		<div class = "container">
+			<div class = "body">
+				@yield('content')
+			</div>
+			<footer class = "footer">
+				<p>All rights reserved © Machine Learning departement(GUC)</p>
+					<img src = "/img/footer/FB-f-Logo__blue_50.png">
+					<img src = "/img/footer/GitHub-Mark-64px.png">
+					<img src = "/img/footer/glyph-logo_May2016.png"> 
+	 				<img src = "/img/footer/Twitter_Logo_Blue.png">
+	 				<img src = "/img/footer/YouTube-icon-full_color.png">
+ 				</p>
+
+			</footer>
+		</div>
+	
+	</body>
+</html>
+
