3c9c0d803a5061c0411e045c5d25dbcaa696263c
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2017-07-19 22:03:44 +0200

    testing the slide show

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,52 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<style>
+   .carousel{
+        width: 100vw;
+    }
+.img{
+    width:100%;
+    object-fit: cover;
+    min-height: 60vh;
+    max-height: 60vh;
+}
+</style>
+<div class="">  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="https://tctechcrunch2011.files.wordpress.com/2015/02/shutterstock_175625024.jpg"  class="img">
+      </div>
+
+      <div class="item">
+        <img src="http://tomtunguz.com/images/ml_icons.jpg"   class="img">
+      </div>
+    
+      <div class="item">
+        <img src="http://hexaring.com/wp-content/uploads/2017/04/m-learning.jpg" class="img">
+      </div>
     </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 </div>
+
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
 		<meta charset="utf-8">
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        
 	    <!-- CSRF Token -->
 	    <meta name="csrf-token" content="{{ csrf_token() }}">
 		<title>Machine Learning-@yield('title')</title>
@@ -63,7 +63,7 @@
 					<img src = "/img/footer/glyph-logo_May2016.png"> 
 	 				<img src = "/img/footer/Twitter_Logo_Blue.png">
 	 				<img src = "/img/footer/YouTube-icon-full_color.png">
- 				</p>
+ 				
 
 			</footer>
 		</div>
