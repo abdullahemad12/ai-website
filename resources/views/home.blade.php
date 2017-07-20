8952be0945d960af2420504ab011113c3a1ee611
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2017-07-20 20:29:26 +0200

    Fixed syntax errors & updated the home page with the basic format

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,4 +37,39 @@
     </div>
 </div>
 
+<div class="text-center">
+    <h1><b>Welcome to the GUC Machine Learning Lab</b></h1>
+</div>
+
+<br>
+<br>
+
+<div class="row container-fluid">
+    <div class="col-md-2"></div>
+
+    <div id="header" class="col-md-8">
+        <div class="frame">
+            <div class="" style="color:#E5DE00;">
+                <h1> <b>Recent Events </b></h1>
+            </div>
+            <div class="row element">
+                <h3 class="elem-title">First Article</h3>
+                <p>Lorem Ipsum awh fnks as i aw was walking down the isle and then i found a tree to hide in then she appeared lorem ipsum again!! ath the car i foun The aoa aeeer ooha aelk faefh akljf hakwfh alwkfjhaw foiawhf awfiawhf awfhawfuhergrenhg erubhnjknbcvmbn .d valkjfaowif awfj nhaeufi aehuef a i uaa q wia aiuw dfahwsiauw uae iaw</p>
+            </div>
+            <hr>
+            <div class="row element">
+                <h3 class="elem-title">Second Article</h3>
+                <p>Lorem Ipsum awh fnks as i aw was walking down the isle and then i found a tree to hide in then she appeared lorem ipsum again!! ath the car i foun The aoa aeeer ooha aelk faefh akljf hakwfh alwkfjhaw foiawhf awfiawhf awfhawfuhergrenhg erubhnjknbcvmbn .d valkjfaowif awfj nhaeufi aehuef a i uaa q wia aiuw dfahwsiauw uae iaw</p>
+            </div>
+            <hr>
+            <div class="row element">
+                <h3 class="elem-title">Third Article</h3>
+                <p>Lorem Ipsum awh fnks as i aw was walking down the isle and then i found a tree to hide in then she appeared lorem ipsum again!! ath the car i foun The aoa aeeer ooha aelk faefh akljf hakwfh alwkfjhaw foiawhf awfiawhf awfhawfuhergrenhg erubhnjknbcvmbn .d valkjfaowif awfj nhaeufi aehuef a i uaa q wia aiuw dfahwsiauw uae iaw</p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-md-2"></div>
+</div>
 @endsection
--- resources/views/projects/add.blade.php
+++ resources/views/projects/add.blade.php
@@ -1,50 +1,44 @@
-@extends('layouts.default')
-
-@section('title')
-Add a project
-@endsection
-
-@section('content')
+@extends('layouts.default') @section('title') Add a project @endsection @section('content')
 <style>
-#file
-{
-	padding-left: 2%;
-}
+    #file {
+        padding-left: 2%;
+    }
+
 </style>
 <h1> Add a new Project!</h1>
 <br>
-<div class = "add_file">
-	<form method="post" action = "{{URL::to('projects/add')}}" class = "form-horizontal" enctype="multipart/form-data">
-			{{ csrf_field() }}
-		<div class = "row">
-			<div class = "col-md-6 col-xs-6">
-				<div class = "form-group  col-md-8 col-xs-8">
-					<input id = "comment_field" class = "form-control" type = "text"  placeholder="Title of the Project" name = "title" autocomplete="off">
-				</div>
-			</div>
-		</div>
-			<div class = "row">
-				<div class = "col-md-6 col-xs-6">
-					<div class = "form-group  col-md-12 col-xs-12">
-						<textarea id = "comment_field" class = "form-control" placeholder="Summary for the project"   name = "summary" autocomplete="off" rows = "10" columns = "30"></textarea> 
-					</div>
-				</div>
-		</div>
+<div class="add_file">
+    <form method="post" action="{{URL::to('projects/add')}}" class="form-horizontal" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <div class="row">
+            <div class="col-md-6 col-xs-6">
+                <div class="form-group  col-md-8 col-xs-8">
+                    <input id="comment_field" class="form-control" type="text" placeholder="Title of the Project" name="title" autocomplete="off">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-xs-6">
+                <div class="form-group  col-md-12 col-xs-12">
+                    <textarea id="comment_field" class="form-control" placeholder="Summary for the project" name="summary" autocomplete="off" rows="10" columns="30"></textarea>
+                </div>
+            </div>
+        </div>
 
-		<div class = "row">
-			<div class = "col-md-4 col-xs-4"">
-				<div class="form-group">
-					<input type="file" name="file" id="file">
-				</div>
-			</div>
-			<div class = "col-md-offset-1 col-md-1 col-xs-offset-1 col-xs-2">
-				 <div class="form-group"> 
-			      		<button type="submit" class="btn btn-default" id = "submit" >Submit</button>
-			  	</div>
-			</div>
-		</div>
+        <div class="row">
+            <div class="col-md-4 col-xs-4">
+                <div class="form-group ">
+                    <input type="file " name="file" id="file">
+                </div>
+            </div>
+            <div class="col-md-offset-1 col-md-1 col-xs-offset-1 col-xs-2">
+                <div class="form-group">
+                    <button type="submit" class="btn btn-default" id="submit ">Submit</button>
+                </div>
+            </div>
+        </div>
 
-  </form>
+    </form>
 </div>
 
 
