f74f1434fe2d63ab0e7886482c2aa3b4cbd634f5
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2017-07-20 18:01:13 +0200

    Trying to commit a small change 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,43 +1,40 @@
-@extends('layouts.default')
-@section('style')
-  <link href="/css/home.css" rel="stylesheet">
-@endsection
-@section('content')
-<div class="">  
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
+@extends('layouts.default') @section('style')
+<link href="/css/home.css" rel="stylesheet"> @endsection @section('content')
+<div class="">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src="https://tctechcrunch2011.files.wordpress.com/2015/02/shutterstock_175625024.jpg"  class="img">
-      </div>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/3d/Code-1076536.jpg" class="img">
+            </div>
 
 
-      <div class="item">
-        <img src="http://tomtunguz.com/images/ml_icons.jpg"   class="img">
-      </div>
-    
-      <div class="item">
-        <img src="http://hexaring.com/wp-content/uploads/2017/04/m-learning.jpg" class="img">
-      </div>
+            <div class="item">
+                <img src="https://pixnio.com/free-images/2017/03/23/2017-03-23-13-49-09.jpg" class="img">
+            </div>
+
+            <div class="item">
+                <img src="https://cdn.pixabay.com/photo/2015/05/26/23/52/technology-785742_960_720.jpg" class="img">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
 </div>
 
 @endsection
