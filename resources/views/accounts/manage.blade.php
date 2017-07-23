eb53760c5fd53407e8adc3e62e0c8a076412c83c
abdullahemad12 <user.name>
2017-07-23 20:53:44 +0200

    checking out

--- resources/views/accounts/manage.blade.php
+++ resources/views/accounts/manage.blade.php
@@ -34,4 +34,63 @@
 	            </form>
 	       	</div>
         </div>
+
+
+    <div class="row" style = "padding-top: 5%;">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Add a new member</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Admin?</label>
+
+                            <div class="col-md-6">
+                                <input type="checkbox" name="admin" value="car">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-md-2 col-md-offset-7">
+                                <button type="submit" class="btn btn-primary">
+                                    Add Member
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
